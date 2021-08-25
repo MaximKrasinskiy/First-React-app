1b160154571faeb7c6dcfe598995b78a5a7944ff
Max <krasinskimax9@gmail.com>
2021-08-25 18:20:12 +0300

    added props

--- src/userInfo.jsx
+++ src/userInfo.jsx
@@ -1,1 +1,15 @@
 
+function UserInfo(props){
+    return (
+        <div>
+            <span>Name: {props.userName}</span> <br />
+            <span>Gender: {props.userGender}</span> <br />
+            <span>Age: {props.userAge}</span> <br />
+            <span>Worked years: {props.yearsWorked}</span> <br />
+            <span>Salary: {props.salary} - thousand rubles !</span>
+
+            <div className="line"></div>
+        </div>
+    )
+}
+export default UserInfo
